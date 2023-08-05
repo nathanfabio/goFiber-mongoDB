78d01fc2fd555b759e7edc4e9f2f53f3ed227527
Nathan Fabio <nathan_3_m@hotmail.com>
2023-08-05 18:10:29 -0300

    feat: methot get

---
+++ users/get.go
@@ -0,0 +1,28 @@
+package users
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/nathanfabio/goFiber-mongoDB/db"
+)
+
+func getAll(c *fiber.Ctx) error {
+	var documents []User
+
+	err := db.Find("users", &documents)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(err.Error())
+	}
+
+	return c.JSON(documents)
+}
+
+func getOne(c *fiber.Ctx) error {
+	var document User
+
+	err := db.FindOne("users", c.Params("id"), &document)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(err.Error())
+	}
+
+	return c.JSON(document)
+}
