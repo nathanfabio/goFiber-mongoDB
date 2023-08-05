7be25bc83809ba6d339f2988dde12cd8ccada2fd
Nathan Fabio <nathan_3_m@hotmail.com>
2023-08-05 11:24:56 -0300

    feat: connection with mongoDB

---
+++ db/connection.go
@@ -0,0 +1,22 @@
+package db
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func getConnection() (*mongo.Client, context.Context) {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	return client, ctx
+}
