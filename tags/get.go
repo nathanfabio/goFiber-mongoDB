5da6a6336e737a25e1fef1dd3ec046730539a587
Nathan Fabio <nathan_3_m@hotmail.com>
2023-08-06 11:26:32 -0300

    feat: method get - tags

---
+++ tags/get.go
@@ -0,0 +1,29 @@
+package tags
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/nathanfabio/goFiber-mongoDB/db"
+)
+
+func getAll(c *fiber.Ctx) error {
+
+	var documents []Tags
+
+	err := db.Find("tags", &documents)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(err.Error())
+	}
+
+	return c.JSON(documents)
+}
+
+func getOne(c *fiber.Ctx) error {
+	var document Tags
+
+	err := db.FindOne("tags", c.Params("id"), &document)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(err.Error())
+	}
+
+	return c.JSON(document)
+}
