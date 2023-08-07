36688481460a0d045d2dd81cd207e1d5ca987865
Nathan Fabio <nathan_3_m@hotmail.com>
2023-08-07 20:20:29 -0300

    feat: AddTask function

--- tags/tasks.go
+++ tags/tasks.go
@@ -1,6 +1,8 @@
 package tags
 
 import (
+	"sort"
+
 	"github.com/nathanfabio/goFiber-mongoDB/db"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -28,4 +30,32 @@ func findOrCreate(name string) (doc Tags, err error) {
 	doc.ID = id
 
 	return
+}
+
+func AddTask(taskID string, names []string) error {
+	for _, name := range names {
+		tag, err := findOrCreate(name)
+		if err != nil {
+			return err
+		}
+
+		i := sort.SearchStrings(tag.Tasks, taskID)
+
+		if i < len(tag.Tasks) && tag.Tasks[i] == taskID {
+			continue	
+		}
+
+		tag.Tasks = append(tag.Tasks, taskID)
+
+		sort.Strings(tag.Tasks)
+
+		result := new(Tag)
+
+		err = db.UpdateOne("tags", tag.ID.Hex(), tag, result)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
